ac09d16a62bdb03d7e829b6ca5044977e6b0f4c9
Abrar Khan <abrar.khan@hee.nhs.uk>
2020-06-03 19:08:08 +0100

    Add next & previous button functionality and CSS styling

--- main.js
+++ main.js
@@ -35,16 +35,16 @@ class song {
 }
 
 const songList = [
-    anything = new song("anything", "Anything", "Jay Z", "anything"),
-    shutup = new song("shutup", "Shut Up", "Stormzy", "shutup"),
-    energy = new song("energy", "Energy", "Drake", "energy"),
-    goCrazy = new song("goCrazy", "Go Crazy", "Chris Brown", "goCrazy"),
-    lifeIsGood = new song("lifeIsGood", "Life is Good", "Future", "lifeIsGood"),
-    ownIt = new song("ownIt", "Own it", "Stormzy", "ownIt"),
-    heat = new song("heat", "Heat", "Chris Brown", "heat"),
-    hello = new song("hello", "Hello", "Adele", "hello"),
-    loyal = new song("loyal", "Loyal", "Chris Brown", "loyal"),
-    memories = new song("memories", "Memories", "Maroon 5", "memories"),
+    anything = new song(0, "Anything", "Jay Z", "anything"),
+    shutup = new song(1, "Shut Up", "Stormzy", "shutup"),
+    energy = new song(2, "Energy", "Drake", "energy"),
+    goCrazy = new song(3, "Go Crazy", "Chris Brown", "goCrazy"),
+    lifeIsGood = new song(4, "Life is Good", "Future", "lifeIsGood"),
+    ownIt = new song(5, "Own it", "Stormzy", "ownIt"),
+    heat = new song(6, "Heat", "Chris Brown", "heat"),
+    hello = new song(7, "Hello", "Adele", "hello"),
+    loyal = new song(8, "Loyal", "Chris Brown", "loyal"),
+    memories = new song(9, "Memories", "Maroon 5", "memories"),
 ]
 
 const createSongList = () => {
@@ -61,11 +61,12 @@ const createSongList = () => {
 listContainer.appendChild(createSongList());
 
 listContainer.onclick = (e) => {
-    // console.log(e);
+    console.log(e);
     const clickedItem = e.target;
     audio.src = '/music/' + clickedItem.innerText + '.mp3';
     songTitle.innerText = clickedItem.innerText;
-    console.log('/music/' + clickedItem.innerText + '.mp3');
+    // console.log('/music/' + clickedItem.innerText + '.mp3');
+    songIndex = songList
     audio.play();
 }
 
@@ -74,6 +75,8 @@ function playSong(){
     songTitle.innerText = `${songList[songIndex].songName} - ${songList[songIndex].artist}`
     
     playBtn.addEventListener('click',function(){
+        console.log("clicked");
+        
         if(!isPlaying){
             audio.play();
             isPlaying = true;
@@ -101,3 +104,31 @@ function playSong(){
        
     })
 }
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songList.length - 1) { songIndex = 0 };
+    audio.src = `music/${songList[songIndex].songName} - ${songList[songIndex].artist}.mp3`;
+    songTitle.innerText = `${songList[songIndex].songName} - ${songList[songIndex].artist}`
+}
+
+function previousSong() {
+    songIndex--;
+    if (songIndex < 0 ) { songIndex = songList.length - 1 };
+    audio.src = `music/${songList[songIndex].songName} - ${songList[songIndex].artist}.mp3`;
+    songTitle.innerText = `${songList[songIndex].songName} - ${songList[songIndex].artist}`
+}
+
+nextBtn.addEventListener('click',function(){
+    nextSong()
+    audio.play()
+    playImage.src = "icons/pause.png";
+})
+
+prevBtn.addEventListener('click',function(){
+    previousSong()
+    audio.play()
+    playImage.src = "icons/pause.png";
+})
+
+/// Shuffle songs
